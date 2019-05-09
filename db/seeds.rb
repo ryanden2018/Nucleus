e0761d095a574875e373f814ba48e6cb4f39b66a
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-09 08:28:18 -0500

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,8 @@ GroupPost.destroy_all
 Pluss.destroy_all
 Request.destroy_all
 
+lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n" * 3
+lorem_ipsum_short = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
 usr1 = User.create(username:"S8n",first_name:"Lucifer",last_name:"Devil",password:"abcdef",age:666,avatar_url:"https://pbs.twimg.com/profile_images/738139405446062081/x0FQk9Yl_400x400.jpg")
 usr2 = User.create(username:"Susan",first_name:"Susan",last_name:"Barnes",age:28,password:"abcdef",avatar_url:"/assets/Quarky.png")
@@ -29,8 +31,12 @@ grp2 = Group.create(name:"Rubles",description:"Rubles",owner_id:usr1.id)
 Subscription.create(user_id:usr1.id,group_id:grp1.id)
 Subscription.create(user_id:usr1.id,group_id:grp2.id)
 
-Post.create(title:"Test post",content:"Test content",user_id:usr1.id,edited:false,private:false)
-Post.create(title:"Another test post",content:"More test content",user_id:usr1.id,edited:false,private:false)
+Subscription.create(user_id:usr3.id,group_id:grp1.id)
+
+pst1 = Post.create(title:"Trees",content:lorem_ipsum,user_id:usr1.id,edited:false,private:false)
+Post.create(title:"Boats",content:lorem_ipsum,user_id:usr1.id,edited:false,private:false)
+Post.create(title:"Kayaks",content:lorem_ipsum,user_id:usr3.id,edited:false,private:true)
+
 
 Block.create(blocker_id:usr2.id,blockee_id:usr1.id)
 
@@ -39,3 +45,7 @@ Friendship.create(user_1_id:usr3.id,user_2_id:usr1.id)
 
 Friendship.create(user_1_id:usr2.id,user_2_id:usr3.id)
 Friendship.create(user_1_id:usr3.id,user_2_id:usr2.id)
+
+Comment.create(post_id:pst1.id,user_id:usr3.id,content:lorem_ipsum_short,edited:false)
+
+GroupPost.create(group_id:grp1.id,post_id:pst1.id)
