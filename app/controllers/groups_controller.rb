500ea0c54a1d8e93f36faefbd28a2fc4bad7c5c2
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-09 15:11:39 -0500

    paging

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,15 @@ class GroupsController < ApplicationController
         redirect_to groups_path
       end
       @posts = @group.posts.uniq.sort_by { |p| (-1)*p.created_at.to_i }
+
+      @recents_number_of_pages = (@posts.length/Post.max_feed_length) + ( @posts.length%Post.max_feed_length==0 ? 0 : 1 )
+      @recents_page_number = params["recents_page"].to_i
+      if @recents_page_number < 1
+        @recents_page_number = 1
+      end
+      if @recents_page_number > @recents_number_of_pages
+        @recents_page_number = @recents_number_of_pages
+      end
     end
 
     def new
