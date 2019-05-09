983a0f25f6aad14c65543dcec7effb20174a5d18
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-09 16:10:47 -0500

    paging

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,7 @@ class GroupsController < ApplicationController
       @posts = @group.posts.uniq.sort_by { |p| (-1)*p.created_at.to_i }
 
       @recents_number_of_pages = (@posts.length/Post.max_feed_length) + ( @posts.length%Post.max_feed_length==0 ? 0 : 1 )
+      @recents_number_of_pages = ( @recents_number_of_pages == 0 ? 1 : @recents_number_of_pages)
       @recents_page_number = params["recents_page"].to_i
       if @recents_page_number < 1
         @recents_page_number = 1
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -6,6 +6,7 @@ class PublicController < ApplicationController
     @posts = Post.where(is_private: false).sort_by { |p| (-1)*p.created_at.to_i }
 
     @recents_number_of_pages = (@posts.length/Post.max_feed_length) + ( @posts.length%Post.max_feed_length==0 ? 0 : 1 )
+    @recents_number_of_pages = ( @recents_number_of_pages == 0 ? 1 : @recents_number_of_pages)
     @recents_page_number = params["recents_page"].to_i
     if @recents_page_number < 1
       @recents_page_number = 1
